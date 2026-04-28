419e4d654f8303c06ef563602f866319d3fa46b1
Debbie Cooper <cooperdeborahk@gmail.com>
2026-04-27 19:17:11 -0500

    Fix Supabase password reset redirect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Navigate, Routes, Route, useLocation } from 'react-router-dom';
 import HomePage from './screens/HomePage';
 import AppPage from './screens/AppPage';
 import JournalPage from './screens/JournalPage';
@@ -7,19 +7,41 @@ import JournalArticlePage from './screens/JournalArticlePage';
 import OurStoryPage from './screens/OurStoryPage';
 import PrivacyPage from './screens/PrivacyPage';
 
+function HomeRoute() {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const hashParams = new URLSearchParams(location.hash.replace(/^#/, ''));
+  const hasAuthCallback =
+    searchParams.has('code') ||
+    searchParams.get('type') === 'recovery' ||
+    hashParams.get('type') === 'recovery' ||
+    searchParams.get('password-reset') === '1';
+
+  if (hasAuthCallback) {
+    return (
+      <Navigate
+        to={`/app${location.search}${location.hash}`}
+        replace
+      />
+    );
+  }
+
+  return <HomePage />;
+}
+
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-  <Route path="/" element={<HomePage />} />
-  <Route path="/app" element={<AppPage />} />
-  <Route path="/our-story" element={<OurStoryPage />} />
-  <Route path="/privacy" element={<PrivacyPage />} />
+        <Route path="/" element={<HomeRoute />} />
+        <Route path="/app" element={<AppPage />} />
+        <Route path="/our-story" element={<OurStoryPage />} />
+        <Route path="/privacy" element={<PrivacyPage />} />
 
-  {/* NEW */}
-  <Route path="/journal" element={<JournalPage />} />
-  <Route path="/journal/:slug" element={<JournalArticlePage />} />
-</Routes>
+        {/* NEW */}
+        <Route path="/journal" element={<JournalPage />} />
+        <Route path="/journal/:slug" element={<JournalArticlePage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
